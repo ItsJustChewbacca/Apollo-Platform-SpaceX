acba604abcc5883e6a6e13dba6dfc9d4fc2b5a1d
Luke Baziw-Grossmann <luke@metallic.localdomain>
2019-01-09 11:26:40 -0800

    setup basic schema/ added comments

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,4 +1,6 @@
+// Importing ApolloServer Class
 const { ApolloServer } = require('apollo-server');
+// Importing Schema 
 const typeDefs = require('./schema');
-
+// Passing schema to new Apollo instance
 const server = new ApolloServer({ typeDefs });
---
+++ start/server/src/schema.js
@@ -0,0 +1,8 @@
+// importing gql from Apollo Server
+const { gql } = require('apollo-server');
+// Assigning variable for schema
+const typeDefs = gql``
+
+
+
+module.exports = typeDefs;

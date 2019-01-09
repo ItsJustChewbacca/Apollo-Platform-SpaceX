98e557538e78851176e329af2a998106f077d417
Luke Baziw-Grossmann <luke@metallic.localdomain>
2019-01-09 11:31:59 -0800

    added first query for launches

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -3,6 +3,19 @@ const { gql } = require('apollo-server');
 // Assigning variable for schema
 const typeDefs = gql``
 
+/*
+	* Define launches query to fetch all upcoming rocket launches
+	* Query returns an array of launches, which will never be null
+	* ! -> This will always return data
+	* Defined Query to fetch a launch by its ID
+	* Query takes an argument of id and returns a single launch
+	* Query users data 
+ */
 
+type Query {
+	launches: [Launch]!
+	launch(id: ID!): Launch
+	me: User
+}
 
 module.exports = typeDefs;

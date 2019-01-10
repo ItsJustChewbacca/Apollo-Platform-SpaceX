62a4a4457bb297201aee6278b186867e0815ed13
Luke Baziw-Grossmann <luke@metallic.localdomain>
2019-01-10 15:35:19 -0800

    resolver mutations

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -1,7 +1,7 @@
 const { paginateResults } = require('./utils');
 
 module.exports = {
-  Query: {
+    Query: {
 	    launches: async (_, { pageSize = 20, after }, { dataSources }) => {
 	      const allLaunches = await dataSources.launchAPI.getAllLaunches();
 	      // we want these in reverse chronological order
@@ -24,7 +24,7 @@ module.exports = {
 	          : false,
 	      };
 	    }
-	}
+	},
 	Mission: {
 	  // make sure the default size is 'large' in case user doesn't specify
 	  missionPatch: (mission, { size } = { size: 'LARGE' }) => {
@@ -50,6 +50,45 @@ module.exports = {
 	        launchIds,
 	      }) || []
 	    );
+	  },
+	},
+	Mutation: {
+		login: async (_, { email }, { dataSources }) => {
+		    const user = await dataSources.userAPI.findOrCreateUser({ email });
+		    if (user) return new Buffer(email).toString('base64');
+		},
+	  bookTrips: async (_, { launchIds }, { dataSources }) => {
+	    const results = await dataSources.userAPI.bookTrips({ launchIds });
+	    const launches = await dataSources.launchAPI.getLaunchesByIds({
+	      launchIds,
+	    });
+
+	    return {
+	      success: results && results.length === launchIds.length,
+	      message:
+	        results.length === launchIds.length
+	          ? 'trips booked successfully'
+	          : `the following launches couldn't be booked: ${launchIds.filter(
+	              id => !results.includes(id),
+	            )}`,
+	      launches,
+	    };
+	  },
+	  cancelTrip: async (_, { launchId }, { dataSources }) => {
+	    const result = dataSources.userAPI.cancelTrip({ launchId });
+
+	    if (!result)
+	      return {
+	        success: false,
+	        message: 'failed to cancel trip',
+	      };
+
+	    const launch = await dataSources.launchAPI.getLaunchById({ launchId });
+	    return {
+	      success: true,
+	      message: 'trip cancelled',
+	      launches: [launch],
+	    };
 	  },
 	},
 };

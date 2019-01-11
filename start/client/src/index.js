2ec1fe4844a631f14b815a56965099c855b09fc9
Luke Baziw-Grossmann <luke@metallic.localdomain>
2019-01-10 16:55:02 -0800

    added querys and setup fragment

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -1,5 +1,11 @@
 import { ApolloClient } from "apollo-client";
+import { ApolloProvider } from 'react-apollo';
 
 const client = new ApolloClient({
   uri: "ItsJustChewbacca-631"
 });
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Pages />
+  </ApolloProvider>, document.getElementById('root'));

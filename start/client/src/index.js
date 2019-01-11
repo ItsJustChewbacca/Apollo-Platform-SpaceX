a7246bcc2ea3faff9165411b7c8c138b91cb8d42
Luke Baziw-Grossmann <luke@metallic.localdomain>
2019-01-10 17:44:21 -0800

    setup queries to handle local data

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -1,5 +1,9 @@
 import { ApolloClient } from "apollo-client";
-import { ApolloProvider } from 'react-apollo';
+import { Query, ApolloProvider } from 'react-apollo';
+import gql from 'graphql-tag';
+
+import Pages from './pages';
+import Login from './pages/login';
 
 const client = new ApolloClient({
   cache,
@@ -15,7 +19,18 @@ const client = new ApolloClient({
   },
 });
 
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+injectStyles();
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Pages />
-  </ApolloProvider>, document.getElementById('root'));
+    <Query query={IS_LOGGED_IN}>
+      {({ data }) => (data.isLoggedIn ? <Pages /> : <Login />)}
+    </Query>
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
